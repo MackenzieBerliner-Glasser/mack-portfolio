bdacb11eb99780f8892bf71208518ab7c3d00cdb
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-01-03 13:07:14 -0800

    formatting?

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from 'react';
 
-
-import Layout from '../components/layout'
+import Layout from '../components/layout';
 import Footer from '../components/footer/footer';
 import About from '../components/about/about';
-
 
 const AboutPage = () => (
   <>
@@ -12,6 +10,6 @@ const AboutPage = () => (
     <About />
     <Footer />
   </>
-)
+);
 
-export default AboutPage
+export default AboutPage;
