17155dddfc214475ea7d1120c7ebd88a4f494c3f
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-01-16 15:00:08 -0800

    technology section added and in progress

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import MainHeader from '../main-header/main-header';
+import Tech from '../tech/tech';
 
 const About = () => {
   return (
     <Fragment>
       <MainHeader />
-      <h1>{'<About />'}</h1>
+      <h3>{'<About />'}</h3>
+      <Tech />
     </Fragment>
   );
 };
---
+++ src/components/tech/tech.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import vueLogo from '../../images/code-logos/vue.png';
+import gatsbyLogo from '../../images/code-logos/gatsby.png';
+import reactLogo from '../../images/code-logos/react.png';
+import reduxLogo from '../../images/code-logos/redux.png';
+import nodeLogo from '../../images/code-logos/node.png';
+import mongoLogo from '../../images/code-logos/mongo.png';
+import postgresLogo from '../../images/code-logos/postgres.png';
+import jestLogo from '../../images/code-logos/jest.png';
+import cssLogo from '../../images/code-logos/css.png';
+import htmlLogo from '../../images/code-logos/html.png';
+import jsLogo from '../../images/code-logos/javascript.png';
+import gitLogo from '../../images/code-logos/git.png';
+
+const Tech = () => {
+  return (
+    <div>
+      <h4>{'{ Technology }'}</h4>
+    </div>
+  );
+};
+
+export default Tech;
+
+
