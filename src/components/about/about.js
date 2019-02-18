e9d8528a33f216a85dd4f1d59176a1a52b4050b4
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-02-18 14:16:17 -0800

    added style to about as well as additional formatting to aside

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,14 +9,29 @@ const About = () => {
       <MainHeader />
       <section className={styles.section}>
         <div className={styles.about}>
-          <h2 className={styles.aboutHeader}>The Bio</h2>
+          <h3 className={styles.aboutHeader}>The Bio</h3>
           <h4 className={styles.aboutSub}>Hi, I&apos;m Mack,</h4>
           <p>
-            A little bit about me... I like to create. That is the long and short of it.
+            Do you see any Teletubbies in here? Do you see a slender plastic tag
+            clipped to my shirt with my name printed on it? Do you see a little
+            Asian child with a blank expression on his face sitting outside on a
+            mechanical helicopter that shakes when you put quarters in it? No?
+            Well, that&apos;s what you see at a toy store. And you must think you&apos;re
+            in a toy store, because you&apos;re here shopping for an infant named
+            Jeb.
           </p>
         </div>
         <aside className={styles.aside}>
-          <h2 className={styles.asideHeader}>Out of Office</h2>
+          <h3 className={styles.asideHeader}>Out of Office</h3>
+          <h4 className={styles.aboutSub}>You can probably find me doing...</h4>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>some content</li>
+            <li className={styles.listItem}>some content</li>
+            <li className={styles.listItem}>some content</li>
+            <li className={styles.listItem}>some content</li>
+            <li className={styles.listItem}>some content</li>
+            <li className={styles.listItem}>some content</li>
+          </ul>
         </aside>
       </section>
       <Tech />
