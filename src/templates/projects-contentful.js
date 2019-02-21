81002e8a6ea6e785766da61574740b7adf6f5f67
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-02-21 11:41:18 -0800

    images pathing properly and navigation between master detail

--- src/templates/projects-contentful.js
+++ src/templates/projects-contentful.js
@@ -6,15 +6,18 @@ import styles from './project-template.module.css';
 class ProjectsContentfulTemplate extends Component {
   render() {
     const project = this.props.data.contentfulProject;
-    console.log(project.title);
+    const image = this.props.data.contentfulProject.image.fluid.src;
+    console.log(image);
     // const siteTitle = this.props.data.site.siteMetadata.title;
     const { previous, next } = this.props.pageContext;
 
     return (
       <Layout>
         <section className={styles.wrapper}>
+          <Link to='/Projects/'>Go Back</Link>
           <h3>{project.title}</h3>
           <p>{project.tech}</p>
+          <img src={image}></img>
           <div
             dangerouslySetInnerHTML={{
               __html: project.description.childContentfulRichText.html,
@@ -53,6 +56,11 @@ export const pageQuery = graphql`
     contentfulProject(slug: { eq: $slug }) {
       title
       tech
+      image {
+        fluid {
+          src
+        }
+      }
       description {
         childContentfulRichText {
           html
