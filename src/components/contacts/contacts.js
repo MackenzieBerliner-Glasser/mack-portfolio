2cbaf8a11b5443b3bcecea672a0b3c71fca64c18
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-04-20 11:59:16 -0700

    added contact page

---
+++ src/components/contacts/contacts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <section>
+      <h1>Hi I am the contacts page</h1>
+    </section>
+  );
+};
+
+export default Contacts;
---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Layout from '../components/layout';
+import Contacts from '../components/contacts/contacts';
+
+
+const ContactPage = () => (
+  <Fragment>
+    <Layout />
+    <Contacts />
+  </Fragment>
+);
+
+export default ContactPage;
+
+
